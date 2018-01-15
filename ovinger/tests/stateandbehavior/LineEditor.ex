632892b47669d3501f7aa8d4d21d64d4845cff4a
Torjus Iveland <torjusti@gmail.com>
2018-01-15 12:48:22 +0100

    Fix LineEditor.ex

--- ovinger/tests/stateandbehavior/LineEditor.ex
+++ ovinger/tests/stateandbehavior/LineEditor.ex
@@ -1,18 +1,18 @@
 <?xml version="1.0" encoding="UTF-8"?>
 <xmi:XMI xmi:version="2.0" xmlns:xmi="http://www.omg.org/XMI" xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance" xmlns:exercise="platform:/plugin/no.hal.learning.exercise.model/model/exercise.ecore" xmlns:jdt="platform:/plugin/no.hal.learning.exercise.jdt/model/jdt-exercise.ecore" xmlns:junit="platform:/plugin/no.hal.learning.exercise.junit/model/junit-exercise.ecore" xmlns:workbench="platform:/plugin/no.hal.learning.exercise.workbench/model/workbench-exercise.ecore">
   <exercise:Exercise>
-    <parts xsi:type="exercise:ExercisePart" title="Line">
+    <parts xsi:type="exercise:ExercisePart" title="LineEditor">
       <tasks xsi:type="exercise:Task">
-        <q xsi:type="exercise:StringQuestion" question="Write source code for the Line class."/>
-        <a xsi:type="jdt:JdtSourceEditAnswer" className="stateandbehavior.Line"/>
+        <q xsi:type="exercise:StringQuestion" question="Write source code for the LineEditor class."/>
+        <a xsi:type="jdt:JdtSourceEditAnswer" className="stateandbehavior.LineEditor"/>
       </tasks>
       <tasks xsi:type="exercise:Task">
-        <q xsi:type="exercise:StringQuestion" question="Run the Line class, to test it."/>
-        <a xsi:type="jdt:JdtLaunchAnswer" className="stateandbehavior.Line"/>
+        <q xsi:type="exercise:StringQuestion" question="Run the LineEditor class, to test it."/>
+        <a xsi:type="jdt:JdtLaunchAnswer" className="stateandbehavior.LineEditor"/>
       </tasks>
       <tasks xsi:type="exercise:Task">
-        <q xsi:type="exercise:StringQuestion" question="Test the Line class, by running the LineTest JUnit test."/>
-        <a xsi:type="junit:JunitTestAnswer" testRunName="stateandbehavior.LineTest"/>
+        <q xsi:type="exercise:StringQuestion" question="Test the LineEditor class, by running the LineEditorTest JUnit test."/>
+        <a xsi:type="junit:JunitTestAnswer" testRunName="stateandbehavior.LineEditorTest"/>
       </tasks>
     </parts>
     <parts xsi:type="exercise:ExercisePart" title="Using Eclipse">

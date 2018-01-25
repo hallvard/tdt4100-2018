bf5bc13a330085681a2e389a163b69f618e8dea0
Hallvard Traetteberg <hal@ntnu.no>
2018-01-25 13:15:33 +0100

    Valideringskode

--- foreksempel/src/time/TimeInterval.java
+++ foreksempel/src/time/TimeInterval.java
@@ -5,18 +5,48 @@ public class TimeInterval {
 	private int startHour = 0;
 	private int startMin = 0;
 	private int duration = 0;
+
+	public boolean isValidHour(int hour) {
+		return (hour >= 0 || hour < 24);
+	}
+	private void checkHour(int hour) {
+		if (! isValidHour(hour)) {
+			return;
+		}
+		// kræsj programmet, men slik at koden kan finne ut av det
+		throw new IllegalArgumentException("Timen må være mellom 0 og 24");
+	}
+	public boolean isValidMin(int min) {
+		return (min >= 0 || min < 60);
+	}
+	private void checkMin(int min) {
+		if (! isValidMin(min)) {
+			return;
+		}
+		// kræsj programmet, men slik at koden kan finne ut av det
+		throw new IllegalArgumentException("Minuttet må være mellom 0 og 60");
+	}
+	
+	public TimeInterval(int startHour, int startMin, int duration) {
+		checkHour(startHour);
+		this.startHour = startHour;
+		this.startMin = startMin;
+		this.duration = duration;
+	}
 	
 	public int getStartHour() {
 		return startHour;
 	}
 	public void setStartHour(int startHour) {
-		if (startHour < 0 || startHour >= 24) {
-			// kræsj programmet, men slik at koden kan finne ut av det
-			throw new IllegalArgumentException("Timen må være mellom 0 og 24");
-		}
+		checkHour(startHour);
 		this.startHour = startHour;
-//		if (startHour >= 0 && startHour < 24) {
-//			this.startHour = startHour;
-//		}
+	}
+	
+	public int getEndHour() {
+		return startHour + duration / 60;
+	}
+	public void setEndHour(int endHour) {
+		checkHour(endHour);
+		this.duration = (endHour - startHour) * 60;
 	}
 }
--- foreksempel/src/time/TimeIntervalProgram.java
+++ foreksempel/src/time/TimeIntervalProgram.java
@@ -3,7 +3,7 @@ package time;
 public class TimeIntervalProgram {
 
 	public static void main(String[] args) {
-		TimeInterval ti1 = new TimeInterval();
+		TimeInterval ti1 = new TimeInterval(12, 15, 105);
 		ti1.setStartHour(-1);
 		System.out.println(ti1.getStartHour());
 	}

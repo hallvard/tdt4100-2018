ebb7c3dbf26307273abf762e60c50163f0989b1f
Vegard Hellem <vhellem@gmail.com>
2018-02-14 16:08:55 +0100

    ovingsforelesning 6

---
+++ foreksempel/src/of6/MinesweeperBoardException.java
@@ -0,0 +1,9 @@
+package of6;
+
+@SuppressWarnings("serial")
+public class MinesweeperBoardException extends IndexOutOfBoundsException{
+	
+	public MinesweeperBoardException(String exceptionMessage) {
+		super(exceptionMessage);
+	}
+}

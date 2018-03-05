3f9d585deb8603a9ebcc8d709c9dcd519cfdd95e
Hallvard Traetteberg <hal@ntnu.no>
2018-03-05 13:06:50 +0100

    Litt innlesingskode

--- foreksempel/src/io/PersonReader.java
+++ foreksempel/src/io/PersonReader.java
@@ -1,8 +1,21 @@
 package io;
 
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.Reader;
+
 public class PersonReader {
 
-	public static void main(String[] args) {
-		
+	public static void main(String[] args) throws IOException {
+		Reader reader = new InputStreamReader(new FileInputStream("/Applications/eclipse/tdt4100-2018-master2/git/tdt4100-2018/foreksempel/src/io/persons.txt"), "UTF-8"); // ISO-8859-1
+		while (true) {
+			int b = reader.read();
+			if (b < 0) {
+				break;
+			}
+			System.out.println((char) b);
+		}
+		reader.close();
 	}
 }

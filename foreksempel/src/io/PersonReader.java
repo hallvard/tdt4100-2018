fe1deef584083a4cf72345b9a6bfcbee288309db
Hallvard Traetteberg <hal@ntnu.no>
2018-03-08 14:23:15 +0100

    PersonReader-eksempel

--- foreksempel/src/io/PersonReader.java
+++ foreksempel/src/io/PersonReader.java
@@ -1,21 +1,59 @@
 package io;
 
+import java.io.BufferedReader;
 import java.io.FileInputStream;
+import java.io.FileNotFoundException;
 import java.io.IOException;
+import java.io.InputStream;
 import java.io.InputStreamReader;
-import java.io.Reader;
+import java.io.UnsupportedEncodingException;
+import java.net.URL;
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.Scanner;
 
 public class PersonReader {
 
-	public static void main(String[] args) throws IOException {
-		Reader reader = new InputStreamReader(new FileInputStream("/Applications/eclipse/tdt4100-2018-master2/git/tdt4100-2018/foreksempel/src/io/persons.txt"), "UTF-8"); // ISO-8859-1
-		while (true) {
-			int b = reader.read();
-			if (b < 0) {
-				break;
+	public Collection<Person> readPersonData(InputStream inputStream) {
+		Collection<Person> result = new ArrayList<Person>();
+		try (BufferedReader reader = new BufferedReader(new InputStreamReader(inputStream))) {
+			while (reader.ready()) {
+				String line = reader.readLine();
+				if (line.startsWith("#")) {
+					continue;
+				}
+				Person person;
+				try {
+					person = new Person();
+					String[] properties = line.split(",");
+					for (String property : properties) {
+						int pos = property.indexOf(':');
+						String propertyName = property.substring(0, pos).trim();
+						String propertyValue = property.substring(pos + 1).trim();
+						if ("name".equals(propertyName)) {
+							person.setName(propertyValue);
+						} else if ("email".equals(propertyName)) {
+							person.setEmail(propertyValue);
+						}
+					}
+					result.add(person);
+				} catch (IllegalArgumentException | StringIndexOutOfBoundsException e) {
+					System.err.println("Syntax error: " + line);
+				}
 			}
-			System.out.println((char) b);
+		} catch (IOException e) {
+			System.err.println(e);
 		}
-		reader.close();
+		return result;
+	}
+	
+	public static void main(String[] args) {
+		PersonReader pr = new PersonReader();
+		try {
+			Collection<Person> persons = pr.readPersonData(new FileInputStream("/Applications/eclipse/tdt4100-2018-master2/git/tdt4100-2018/foreksempel/src/io/persons.txt"));
+			System.out.println(persons);
+		} catch (FileNotFoundException e) {
+			System.err.println(e);
+		}
 	}
 }

5240579daef0fcbed69cc264cf5ce05770a3d2fb
Hallvard Traetteberg <hal@ntnu.no>
2018-04-05 13:10:38 +0200

    Point.equals

---
+++ foreksempel/src/arv/Point2d.java
@@ -0,0 +1,28 @@
+package arv;
+
+import java.util.Arrays;
+import java.util.Collection;
+
+public class Point2d {
+
+	public final double x, y;
+
+	public Point2d(final double x, final double y) {
+		this.x = x;
+		this.y = y;
+	}
+
+	@Override
+	public boolean equals(final Object obj) {
+		if (! (obj instanceof Point2d)) {
+			return false;
+		}
+		final Point2d other = (Point2d) obj;
+		return this.x == other.x && this.y == other.y;
+	}
+
+	public static void main(final String[] args) {
+		final Collection<Point2d> points = Arrays.asList(new Point2d(1, 2), new Point2d(3, 4));
+		System.out.println(points.contains(new Point2d(3, 5)));
+	}
+}

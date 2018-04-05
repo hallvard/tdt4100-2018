955aaa19af4546e4ca96e720a9f9185798c5ad6a
Hallvard Traetteberg <hal@ntnu.no>
2018-04-05 12:15:15 +0200

    Arv-eksempler

---
+++ foreksempel/src/arv/Person.java
@@ -0,0 +1,28 @@
+package arv;
+
+public class Person {
+
+	private String name;
+
+	public Person(final String name) {
+		setName(name);
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(final String name) {
+		checkName(name);
+		this.name = name;
+	}
+
+	private void checkName(final String name) {
+		for (int i = 0; i < name.length(); i++) {
+			final char c = name.charAt(i);
+			if (! (Character.isLetter(c) || " -".indexOf(c) >= 0)) {
+				throw new IllegalArgumentException(name + " is an illegal name");
+			}
+		}
+	}
+}

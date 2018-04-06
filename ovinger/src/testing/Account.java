4383bdc31bb2795ac0fd4f2a26fb7b43620fa7d0
Torjus Iveland <torjusti@gmail.com>
2018-04-06 10:30:10 +0200

    Add testing-files for excercise 10

---
+++ ovinger/src/testing/Account.java
@@ -0,0 +1,54 @@
+package testing;
+
+public class Account {
+
+	private double balance;
+	private double interestRate;
+
+	public Account(double balance, double interestRate) {
+		validateAmount(balance);
+		validateInterestRate(interestRate);
+		this.balance = balance;
+		this.interestRate = interestRate;
+	}
+
+	private void validateInterestRate(double interestRate) {
+		if (interestRate < 0) {
+			throw new IllegalStateException("Account can not have negative interest rate");
+		}
+	}
+
+	private void validateAmount(double amount) {
+		if (amount < 0) {
+			throw new IllegalArgumentException("Negative amount");
+		}
+	}
+
+	public double getBalance() {
+		return this.balance;
+	}
+
+	public double getInterestRate() {
+		return this.interestRate;
+	}
+
+	public void setInterestRate(double interestRate) {
+		validateInterestRate(interestRate);
+		this.interestRate = interestRate;
+	}
+
+	public void deposit(double amount) {
+		validateAmount(amount);
+		this.balance += amount;
+	}
+
+	public void withdraw(double amount) {
+		validateAmount(amount);
+		balance -= amount;
+	}
+
+	public void addInterest() {
+		this.balance += this.balance * this.interestRate;
+	}
+
+}
---
+++ ovinger/tests/testing/Card.ex
@@ -0,0 +1,33 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<xmi:XMI xmi:version="2.0" xmlns:xmi="http://www.omg.org/XMI" xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance" xmlns:exercise="platform:/plugin/no.hal.learning.exercise.model/model/exercise.ecore" xmlns:jdt="platform:/plugin/no.hal.learning.exercise.jdt/model/jdt-exercise.ecore" xmlns:junit="platform:/plugin/no.hal.learning.exercise.junit/model/junit-exercise.ecore" xmlns:workbench="platform:/plugin/no.hal.learning.exercise.workbench/model/workbench-exercise.ecore">
+  <exercise:Exercise>
+    <parts xsi:type="exercise:ExercisePart" title="CardTest">
+      <tasks xsi:type="exercise:Task">
+        <q xsi:type="exercise:StringQuestion" question="Write JUnit source code for the CardTest class."/>
+        <a xsi:type="jdt:JdtSourceEditAnswer" className="testing.CardTest"/>
+      </tasks>
+      <tasks xsi:type="exercise:Task">
+        <q xsi:type="exercise:StringQuestion" question="Run the JUnit test for the CardTest class."/>
+        <a xsi:type="junit:JunitTestAnswer" testRunName="testing.CardTest"/>
+      </tasks>
+    </parts>
+    <parts xsi:type="exercise:ExercisePart" title="Using Eclipse">
+      <tasks xsi:type="exercise:Task">
+        <q xsi:type="exercise:StringQuestion" question="Use breakpoints to debug code."/>
+        <a xsi:type="workbench:DebugEventAnswer" action="suspend.breakpoint"/>
+      </tasks>
+      <tasks xsi:type="exercise:Task">
+        <q xsi:type="exercise:StringQuestion" question="Use the debug command Step Over"/>
+        <a xsi:type="workbench:CommandExecutionAnswer" elementId="org.eclipse.debug.ui.commands.StepOver" action="executeSuccess"/>
+      </tasks>
+      <tasks xsi:type="exercise:Task">
+        <q xsi:type="exercise:StringQuestion" question="Use the debug command Step Into"/>
+        <a xsi:type="workbench:CommandExecutionAnswer" elementId="org.eclipse.debug.ui.commands.StepInto" action="executeSuccess"/>
+      </tasks>
+      <tasks xsi:type="exercise:Task">
+        <q xsi:type="exercise:StringQuestion" question="Use the Variables view"/>
+        <a xsi:type="workbench:PartTaskAnswer" elementId="org.eclipse.debug.ui.VariableView" action="activated"/>
+      </tasks>
+    </parts>
+  </exercise:Exercise>
+</xmi:XMI>
